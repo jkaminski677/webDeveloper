304ce5bcb2556d980089f106048d085f4112b829
Jan <jkaminski677@gmail.com>
2023-02-25 02:07:25 +0100

    test9

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const Xvar = Math.random() * (-100 - (-200)) + (-200) ;
-// console.log(Xvar);
-const X = document.querySelector('#circles');
-X.style.setProperty('--animation-X', Xvar +'px');
+// For background animations
+// const Xvar = Math.random() * (-100 - (-200)) + (-200) ;
+// // console.log(Xvar);
+// const X = document.querySelector('#circles');
+// X.style.setProperty('--animation-X', Xvar +'px');
 
 
-const Yvar = Math.random() * (-100 - (-200)) + (-200) ;
-// console.log(Yvar);
-const Y = document.querySelector('#circles');
-Y.style.setProperty('--animation-Y', Yvar +'px');
+// const Yvar = Math.random() * (-100 - (-200)) + (-200) ;
+// // console.log(Yvar);
+// const Y = document.querySelector('#circles');
+// Y.style.setProperty('--animation-Y', Yvar +'px');
 
 
 function containsSpecialChars(str) {
